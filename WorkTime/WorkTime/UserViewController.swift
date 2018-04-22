9e6c48488705982067c8c0af914d03cf5acf641c
Szymon Szysz <sszysz@netizens.pl>
2018-04-22 19:01:57 +0200

    Add user view

---
+++ WorkTime/WorkTime/UserViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UserViewController.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 22.04.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var profilePictureImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       UserViewConfiguration()
+        
+    
+    }
+
+    func UserViewConfiguration() {
+        
+        whiteView.layer.cornerRadius = 10
+        whiteView.layer.shadowOffset = CGSize(width: -10, height: 30)
+        whiteView.layer.shadowColor = UIColor.black.cgColor
+        whiteView.layer.shadowOpacity = 0.4
+        profilePictureImageView.layer.cornerRadius = 10
+        profilePictureImageView.layer.backgroundColor = UIColor.white.withAlphaComponent(0.7).cgColor
+        
+    }
+}
