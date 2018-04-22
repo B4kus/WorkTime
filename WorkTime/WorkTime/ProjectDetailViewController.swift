cd62b38f19e98b55635be3a0608c9fbb47581c13
Szymon Szysz <sszysz@netizens.pl>
2018-04-22 12:41:14 +0200

    Add detail project view

---
+++ WorkTime/WorkTime/ProjectDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ProjectDetailViewController.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 22.04.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class ProjectDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
+
+
+
+extension ProjectDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 4
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        
+        return cell
+        
+    } 
+}
