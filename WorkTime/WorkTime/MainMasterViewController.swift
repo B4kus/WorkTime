d90a8dd965a6562d4b463df0262dca878acb152e
Szymon Szysz <sszysz@netizens.pl>
2018-04-22 12:29:57 +0200

    Add mainTableView

---
+++ WorkTime/WorkTime/MainMasterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainMasterViewController.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 22.04.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class MainMasterViewController: UIViewController {
+
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
+
+extension MainMasterViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 4
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+        
+    }
+}
