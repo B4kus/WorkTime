501cc7d14a58ca635452eb42f967bf77497a4901
Szymon Szysz <sszysz@netizens.pl>
2018-04-22 11:32:56 +0200

    Front Design

--- WorkTime/WorkTime/ViewController.swift
+++ WorkTime/WorkTime/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var whiteView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInOutlet: UIButton!
+    @IBOutlet weak var registerOutlet: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        loginConfiguration()
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loginConfiguration() {
+        
+        
+        whiteView.layer.cornerRadius = 10
+        whiteView.layer.shadowOffset = CGSize(width: -10, height: 30)
+        whiteView.layer.shadowColor = UIColor.black.cgColor
+        whiteView.layer.shadowOpacity = 0.4
+        
+        emailTextField.layer.cornerRadius = 6
+        emailTextField.layer.borderWidth = 0.5
+        emailTextField.layer.backgroundColor = UIColor.black.withAlphaComponent(0.1).cgColor
+       
+        passwordTextField.layer.cornerRadius = 6
+        passwordTextField.layer.borderWidth = 0.5
+        passwordTextField.layer.backgroundColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        
+        logInOutlet.layer.borderWidth = 0.5
+        logInOutlet.layer.cornerRadius = 6
+        
+        registerOutlet.layer.borderWidth = 0.5
+        registerOutlet.layer.cornerRadius = 6
+    
     }
-
-
+    
+    
+    
 }
 
