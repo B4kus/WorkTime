a048c91710044b6709ffe07253f77c5e96e1ff11
Szymon Szysz <sszysz@netizens.pl>
2018-04-26 18:50:11 +0200

    Add User main view

---
+++ WorkTime/WorkTime/UserMainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  UserMainViewController.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 26.04.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class UserMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
+extension UserMainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "projectCell", for: indexPath)
+        
+        return cell
+        
+    }
+}
