2109d0117c114e7697310cd978f8e3144166971a
Szymon Szysz <sszysz@netizens.pl>
2018-04-26 19:34:14 +0200

    Add user task view

---
+++ WorkTime/WorkTime/UserTaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserTaskViewController.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 26.04.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import UIKit
+
+class UserTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
