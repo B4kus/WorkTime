a590ca4c22fe9e641f6c63d6359faf40f86bb34a
Szymon Szysz <sszysz@netizens.pl>
2018-05-01 12:10:50 +0200

    Add project model

---
+++ WorkTime/WorkTime/Models/Project.swift
@@ -0,0 +1,38 @@
+//
+//  Project.swift
+//  WorkTime
+//
+//  Created by Szymon Szysz on 01.05.2018.
+//  Copyright © 2018 Szymon Szysz. All rights reserved.
+//
+
+import Foundation
+
+
+public final class Project: NSObject{
+    
+    
+    @objc dynamic var name: String?
+    @objc dynamic var client: String?
+    @objc dynamic var platform: String?
+    @objc dynamic var api: String?
+    @objc dynamic var time = 0
+    @objc dynamic var info: String?
+    @objc dynamic var extraInfo: String?
+    @objc dynamic var project_id: String?
+    
+    
+    private struct SerializationKeys {
+        
+        static let name = "name"
+        static let client = "client"
+        static let platform = "platform"
+        static let api = "api"
+        static let time = "time"
+        static let info = "info"
+        static let extraInfo = "extraInfo"
+        static let project_id = "project_id"
+        
+    }
+    
+}
